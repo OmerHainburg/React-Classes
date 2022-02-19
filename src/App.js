914bc66c4e8ffd4c8d822855335519c89f672c0e
Your Name <you@example.com>
2022-02-19 22:31:48 +0100

    Sel-ClosingTags

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const h1 = <h1>ClassvsClassName</h1>;
+  const myDiv = <div className="big">I AM A BIG DIV</div>;
+  const h1SelfClosTags = <h1>Self-Closing Tags</h1>;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {h1}
+        {myDiv}
+        {h1SelfClosTags}
       </header>
     </div>
   );
