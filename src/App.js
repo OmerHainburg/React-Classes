b74220f68b7c3308dac52cafe22caaaa3b94397b
Your Name <you@example.com>
2022-02-19 22:39:21 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ function App() {
   const h1 = <h1>ClassvsClassName</h1>;
   const myDiv = <div className="big">I AM A BIG DIV</div>;
   const h1SelfClosTags = <h1>Self-Closing Tags</h1>;
+  const profile = (
+    <div>
+      <h1>I AM JENKINS</h1>
+      <img src="omer.jpg" alt="omer" />
+      <article>
+        I LIKE TO SIT
+        <br />
+        Omer IS MY NAME
+        <br />
+        THANKS HA LOT
+      </article>
+    </div>
+  );
   return (
     <div className="App">
       <header className="App-header">
         {h1}
         {myDiv}
         {h1SelfClosTags}
+        {profile}
       </header>
     </div>
   );
